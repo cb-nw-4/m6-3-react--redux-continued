59a2280db33b3961d7ac42bd89d01e89cc57daab
chris shino <chris.shino@hotmail.com>
2021-02-02 20:19:20 -0500

    done up to # 3

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import GlobalStyles from "../GlobalStyles";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
-  return <Router>TODO</Router>;
+  return (
+    <GlobalStyles>
+      <Router>TODO</Router>;
+    </GlobalStyles>
+  );
 };
 
 export default App;
