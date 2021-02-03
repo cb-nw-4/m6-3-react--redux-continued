c01a135abe8f4ce80cf9a533a441f37d04787fd9
chris shino <chris.shino@hotmail.com>
2021-02-02 21:35:55 -0500

    done # 5

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import GlobalStyles from "../GlobalStyles";
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import ArtistPage from "./ArtistPage";
 
+const DEFAULT_ARTIST_ID = "7BMccF0hQFBpP6417k1OtQ";
 
 const App = () => {
   return (
     <GlobalStyles>
-      <Router>TODO</Router>;
+      <Router>
+        <Switch>
+          <Route exact path="/artists/:id">
+            <ArtistPage />
+          </Route>
+          <Redirect to={`/artists/${DEFAULT_ARTIST_ID}`} />
+        </Switch>
+      </Router>
+      ;
     </GlobalStyles>
   );
 };
---
+++ src/components/App/ArtistPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ArtistPage = () => {
+  return (
+    <div>
+      HELLOOOO
+    </div>
+  )
+}
+
+export default ArtistPage
