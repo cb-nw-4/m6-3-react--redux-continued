b117e07341ed632b1e660a6b999848e71c144305
chris shino <chris.shino@hotmail.com>
2021-02-04 18:13:43 -0500

    working on hard part of # 7

--- src/components/App/ArtistPage.js
+++ src/components/App/ArtistPage.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import {useParams} from 'react-router-dom'
+import {fetchArtistProfile} from '../../helpers/api-helpers'
 
 const ArtistPage = () => {
+  const {id} = useParams()
   const accessToken = useSelector((state) => state.auth.token);
+
+  
+  fetchArtistProfile(accessToken, id)
+
   return <div>{accessToken}</div>;
 };
 
