6c82fb6c9649fe4ee4c58ec01f47337200fd9275
Doug Wright <dw@nof49.ca>
2021-02-04 12:12:12 -0500

    Completed exercise 6

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import {
   BrowserRouter as Router,
   Switch,
@@ -6,12 +7,32 @@ import {
   Redirect
 } from 'react-router-dom';
 
-import GlobalStyles from './GlobalStyles';
+import GlobalStyles from '../GlobalStyles';
 import ArtistRoute from '../ArtistRoute';
+import {
+  requestAccessToken,
+  receiveAccessToken,
+  receiveAccessTokenError
+} from '../../actions';
 
 const DEFAULT_ARTIST_ID = '11Y54BxlxC3UIAUkU2eadQ';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  // Grab an access token from the backend
+  useEffect(() => {
+    dispatch(requestAccessToken());
+    fetch('/spotify_access_token')
+      .then((res) => res.json())
+      .then((json) => {
+        dispatch(receiveAccessToken(json.access_token));
+      })
+      .catch((err) => {
+        dispatch(receiveAccessTokenError());
+      })
+  }, []);
+
   return (
     <>
       <GlobalStyles />
--- src/components/ArtistRoute.js
+++ src/components/ArtistRoute.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const ArtistRoute = () => {
+  const accessToken = useSelector((state) => state.auth.token);
+
   return (
-    <>Hello</>
+    <>
+      {accessToken}
+    </>
   );
 };
 
