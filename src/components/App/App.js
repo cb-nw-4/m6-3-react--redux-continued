6191c12dfc1a684a51952ed706b88b54c49abfa1
chris shino <chris.shino@hotmail.com>
2021-02-04 17:52:52 -0500

    done # 6

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import GlobalStyles from "../GlobalStyles";
 import {
   BrowserRouter as Router,
@@ -7,17 +7,35 @@ import {
   Redirect,
 } from "react-router-dom";
 import ArtistPage from "./ArtistPage";
+import {requestAccessToken, receiveAccessToken, receiveAccessTokenError} from '../actions'
+import {useDispatch} from 'react-redux'
+
 
 const App = () => {
+  const dispatch = useDispatch()
   const DEFAULT_ARTIST_ID = "7BMccF0hQFBpP6417k1OtQ";
+
+  useEffect(() => {
+    const fetchToken = async() => {
+      dispatch(requestAccessToken())
+      try {
+        const body = await fetch('/spotify_access_token')
+        const bodyJson = await body.json()
+        const token = bodyJson.access_token
+        dispatch(receiveAccessToken(token))
+      }
+      catch (e) {
+        console.error(e)
+        dispatch(receiveAccessTokenError())
+      }
+    }
+    fetchToken()
+  }, [])
   return (
     <>
       <GlobalStyles/>
       <Router>
         <Switch>
-          <Route exact path="/">
-            <div>hello</div>
-          </Route>
           <Route exact path="/artists/:id">
             <ArtistPage />
           </Route>
--- src/components/App/ArtistPage.js
+++ src/components/App/ArtistPage.js
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 const ArtistPage = () => {
-  return (
-    <div>
-      HELLOOOO
-    </div>
-  )
-}
+  const accessToken = useSelector((state) => state.auth.token);
+  return <div>{accessToken}</div>;
+};
 
-export default ArtistPage
+export default ArtistPage;
