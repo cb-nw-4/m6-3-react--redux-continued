d19c989f0879a7ca55b52c24f3bd67bc20ee9ec1
“Winnie <chanwinnie.wc@gmail.com>
2021-02-04 20:31:15 -0500

    Exercise 2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import GlobalStyles from "./GlobalStyles";
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => {
-  return <Router>TODO</Router>;
+  return <Router><GlobalStyles />TODO</Router>;
 };
 
 export default App;
