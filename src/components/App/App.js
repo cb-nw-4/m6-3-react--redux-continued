a62250c7f9890342a8ca5fb69bd2d327a0de0f4a
Vanessa Chan <chw.vanessa@gmail.com>
2021-02-05 18:24:03 -0500

    Ex-5

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 import GlobalStyles from "../GlobalStyles";
+import ArtistRoute from '../ArtistRoute';
+
+const DEFAULT_ARTIST_ID='6fWVd57NKTalqvmjRd2t8Z'; //24kGoldn
+const defaultPath = "/artists/"+DEFAULT_ARTIST_ID;
 
 const App = () => {
   return <Router>
     <GlobalStyles/>
+    <Switch>
+    <Route exact path='/'>
+        <Redirect to={defaultPath}/>
+      </Route>
+      <Route exact path='/artists/:id'>
+        <ArtistRoute/>
+      </Route>
+    </Switch>
   </Router>;
 };
 
---
+++ src/components/ArtistRoute.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const ArtistRoute = () => {
+  return (
+  <>
+  <p>Artist route</p>
+  </>
+  
+);};
+
+
+
+export default ArtistRoute;
