94f6ad0cb8385520addda1355eb56f87d267e632
“Winnie <chanwinnie.wc@gmail.com>
2021-02-06 01:33:55 -0500

    Exercise 5

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import GlobalStyles from "./GlobalStyles";
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import ArtistRoute from "./ArtistRoute";
+
+const DEFAULT_ARTIST_ID = "06HL4z0CvFAxyc27GXpf02";
 
 const App = () => {
-  return <Router><GlobalStyles />TODO</Router>;
+
+  return (
+    <Router>
+      <GlobalStyles />
+      <Switch>
+        <Route exact path="/">
+          <Redirect to={`/artists/${DEFAULT_ARTIST_ID}`} />
+        </Route>
+        <Route exact path="/artists/:id">
+          <ArtistRoute />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/App/ArtistRoute.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ArtistRoute = () => {
+
+    return (
+        <>
+            "TODO"
+        </>
+    )
+}
+
+export default ArtistRoute;
